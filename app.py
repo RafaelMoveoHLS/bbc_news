912dc0231c97b614fedc87a23b72de20b46ab6fa
RafaelMoveoHLS <rafaela@moveohls.com>
2024-10-08 13:38:55 +0300

    App load news on setup + Added user routs

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import pandas as pd
-from typing import Any
+from typing import Any, Dict
 from pymongo import MongoClient
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+try:
+    # MongoDB setup
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["task1"]
+    collection = db["bbc_news"]
+    print("\033[95m Connected to MongoDB \033[0m")
+except Exception as e:
+    raise RuntimeError(f"Error connecting to MongoDB: {str(e)}")
+
 
 def load_bbc_news_data(file_path: str) -> pd.DataFrame:
     """
     Load BBC news data from a CSV file into a pandas DataFrame.
-    
+
     Args:
         file_path (str): The file path to the CSV file.
-    
+
     Returns:
         pd.DataFrame: The loaded DataFrame with the news data.
     """
@@ -17,15 +30,15 @@ def load_bbc_news_data(file_path: str) -> pd.DataFrame:
         return df
     except Exception as e:
         raise RuntimeError(f"Error loading the data: {str(e)}")
-    
+
 
 def filter_news_first_half_2024(df: pd.DataFrame) -> pd.DataFrame:
     """
     Filter the news data for the first half of 2024.
-    
+
     Args:
         df (pd.DataFrame): The DataFrame containing the news data.
-    
+
     Returns:
         pd.DataFrame: The filtered DataFrame with news from the first half of 2024.
     """
@@ -36,14 +49,15 @@ def filter_news_first_half_2024(df: pd.DataFrame) -> pd.DataFrame:
         # Filter news for the first half of 2024
         start_date = pd.to_datetime('2024-01-01')
         end_date = pd.to_datetime('2024-06-30')
-        filtered_df = df[(df['pubDate'] >= start_date) & (df['pubDate'] < end_date)]
+        filtered_df = df[(df['pubDate'] >= start_date)
+                         & (df['pubDate'] < end_date)]
         return filtered_df
-    
+
     except Exception as e:
         raise RuntimeError(f"Error filtering the data: {str(e)}")
 
 
-def get_mongo_client(uri: str="mongodb://localhost:27017/" ) -> MongoClient:
+def get_mongo_client(uri: str = "mongodb://localhost:27017/") -> MongoClient:
     """
     Establish a connection to a MongoDB client.
 
@@ -60,43 +74,60 @@ def get_mongo_client(uri: str="mongodb://localhost:27017/" ) -> MongoClient:
         raise RuntimeError(f"Error connecting to MongoDB: {str(e)}")
 
 
-def insert_news_to_mongo(db_name: str, collection_name: str, data: pd.DataFrame, client: MongoClient) -> None:
+def insert_news_to_mongo(collection: any, data: pd.DataFrame) -> None:
     """
     Inserts the filtered news data into the MongoDB collection.
-    
+
     Args:
-        db_name (str): The database name.
-        collection_name (str): The collection name.
+        collection (MongoClient.database.collection): The collection name.
         data (pd.DataFrame): The filtered news DataFrame.
-        client (MongoClient): The MongoDB client.
     """
     try:
-        db = client[db_name]
-        collection = db[collection_name]
-        
-        # Convert DataFrame to dictionary
         news_dict = data.to_dict(orient='records')
-        
+
         # Insert data into collection
         if news_dict:
             collection.insert_many(news_dict)
+            print(f"\033[92m Data loaded successfully into MongoDB.\033[0m [{len(news_dict)} documents]")
         else:
             raise ValueError("No data to insert.")
     except Exception as e:
         raise RuntimeError(f"Error inserting data into MongoDB: {str(e)}")
 
 
-if __name__ == "__main__":
-    # Load the data
-    df = load_bbc_news_data('bbc_news_data/bbc_news.csv')
+@app.post("/count")
+async def count_rows(query: Dict[str, Any]):
+    """
+    API route that receives a JSON payload with key-value pairs 
+    and returns the count of rows in MongoDB that match the query.
 
-    # Filter the news
-    filtered_df = filter_news_first_half_2024(df)
+    Args:
+        query (QueryModel): The query parameters in the request body.
 
-    # Connect to MongoDB
-    client = get_mongo_client()
+    Returns:
+        dict: A dictionary containing the count of matching rows.
+    """
+    try:
+        print(query)
 
-    # Insert the filtered data into MongoDB
-    insert_news_to_mongo(db_name="task1", collection_name="bbc_news", data=filtered_df, client=client)
+        # Perform the MongoDB query
+        count = collection.count_documents(query)
 
-    print("News data inserted successfully into MongoDB.")
+        # Return the count
+        return {"count": count}
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error querying database: {str(e)}")
+
+
+@app.on_event("startup")
+def load_data():
+    # Check if collection is empty
+    if collection.count_documents({}) == 0:
+        print("\033[93m Loading data from CSV to MongoDB... \033[0m")
+        df = load_bbc_news_data('bbc_news_data/bbc_news.csv')
+        filtered_df = filter_news_first_half_2024(df)
+        insert_news_to_mongo(collection, data=filtered_df)
+    else:
+        print("\033[96m No data to load. MongoDB collection is not empty. \033[0m")
