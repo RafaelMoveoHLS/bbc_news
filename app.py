5b97ceffd4611df01299078528f07063cc274b27
RafaelMoveoHLS <rafaela@moveohls.com>
2024-11-20 13:35:40 +0200

    Disable data loader

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from pymongo import MongoClient
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from services.dataloader import load_data
+# from services.dataloader import load_data
 from services.logger import get_logger
 from routers.news_router import router as news_router
 
@@ -9,15 +8,16 @@ from routers.news_router import router as news_router
 logger = get_logger()
 
 # Lifespan event handler
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup actions
     logger.info("App is starting up...")
-    load_data()
+    # load_data()
     yield
     # Shutdown actions
     logger.info("App is shutting down...")
 
 app = FastAPI(lifespan=lifespan)
-
 app.include_router(news_router, prefix="/news", tags=["News"])
